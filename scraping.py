653ec871ee1901cf02bcda74fff1157bc722db62
carlaLopez <carla.lopezsanchez20@gmail.com>
2024-05-06 15:06:17 -0400

    adding data to csv

--- scraping.py
+++ scraping.py
@@ -3,30 +3,42 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from io import StringIO
+import time
 
 standings_url = "https://fbref.com/en/comps/12/La-Liga-Stats"
 
-data = requests.get(standings_url)
+years = list(range(2024, 2020, -1))
 
-data.text
+all_stats = []
 
-soup = BeautifulSoup(data.text)
+for year in years:
+    data = requests.get(standings_url)
+    soup = BeautifulSoup(data.text)
+    standing_table  = soup.select('table.stats_table')[0]
+    links = standing_table.find_all('a')
+    links = [l.get('href') for l in links]
+    links = [l for l in links if '/squads' in l]
+    team_urls = [f"https://fbref.com{l}" for l in links]
 
-standing_table  = soup.select('table.stats_table')[0]
+    previous_season = soup.select("a.prev")[0].get("href")
+    standings_url = f"https://fbref.com{previous_season}"
 
-links = standing_table.find_all('a')
+    for team in team_urls:
+        team_name = team.split("/")[-1].replace("-Stats","").replace("-"," ")
 
-links = [l.get('href') for l in links]
-
-links = [l for l in links if '/squads' in l]
-
-team_urls = [f"https://fbref.com{l}" for l in links]
-
-team_url = team_urls[2]
-data = requests.get(team_url)
-
-players_stats = pd.read_html(StringIO(data.text),match = "Standard Stats ")
+        data = requests.get(team)
+        players_stats = pd.read_html(StringIO(data.text),match = "Standard Stats ")[0]
 
 
+        players_stats.columns =  players_stats.columns.droplevel()
 
-players_stats[0].columns.droplevel()
+        players_stats["Season"] = year
+        players_stats["Team"] = team_name
+        all_stats.append(players_stats)
+        time.sleep(1)
+    
+
+stats_df = pd.concat(all_stats)
+
+stats_df.to_csv("stats.csv")
+
