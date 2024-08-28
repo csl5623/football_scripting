0c956f57037d729556d2cbed5cadd0e6f57f3ffe
Lopez <calopez@vicr.com>
2024-08-28 16:37:04 -0400

    entity files

--- futbol/src/main/java/com/api/futbol/dto/Player.java
+++ futbol/src/main/java/com/api/futbol/dto/Player.java
@@ -2,4 +2,7 @@ package com.api.futbol.dto;
 
 public class Player {
     
+    private String name;
+    private String Nationality;
+
 }
--- futbol/src/main/java/com/api/futbol/model/PlayerEntity.java
+++ futbol/src/main/java/com/api/futbol/model/PlayerEntity.java
@@ -1,5 +1,8 @@
 package com.api.futbol.model;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class PlayerEntity {
     
 }
